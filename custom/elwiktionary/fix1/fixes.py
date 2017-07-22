c5e7e4770393b413434c25dda6e0f02c0099d7e3
Ηλιάδης Ηλίας <Xoristzatziki@users.noreply.github.com>
2017-07-22 09:20:34 +0300

    Update fixes.py

--- custom/elwiktionary/fix1/fixes.py
+++ custom/elwiktionary/fix1/fixes.py
@@ -12,8 +12,42 @@ nameAre = '(ανδρικό\s*όνομα|ανδρικό\s*\[\[όνομα]])'
 def test():
     print(__file__)
 
+def fixthis(wikitext, languages, parts, basic):
+    #print('inside')
+    nameAAllre = nameAre + '|\[\[Κατηγορία:Ανδρικά ονόματα \('
+    #print('inside 2')
+    if '[[Κατηγορία:Ανδρικά ονόματα (ελληνικά)]]' in wikitext and '{{ονομαΑ}}' in wikitext:
+        return wikitext.replace('[[Κατηγορία:Ανδρικά ονόματα (ελληνικά)]]', '').rstrip(), ''
+    matched = re.search(nameAAllre, wikitext)
+    #print('inside 3')
+    if matched:
+        #print('inside 4')
+        sections = basic.getsections(wikitext, languages, parts)
+        if sections[0]['garbage'] != '':
+            print('HAS GARBAGE')
+            return wikitext, sections[0]['garbage'] 
+        #if '{{τ|en|Democritus}}' in wikitext:
+            #print(sections)
+        #print(sections)
+        newsections, haschanges = fixthis2(sections, languages, parts)
+        #print('haschanges', haschanges)
+        newtext = newsections[0]['content']
+        if haschanges:
+            #μόνο αν έχει αλλαγές
+            newsections = basic.fixseparators(newsections)
+            newtext = newsections[0]['content'] 
+            for xcounter in range(1, len(newsections)):
+                depth = newsections[xcounter]['depth']
+                newtext += "=" * depth + newsections[xcounter]['originaltitle'] + "=" * depth + '\n'
+                newtext += newsections[xcounter]['content']
+            newtext = newtext.rstrip()
+            #print("return newtext, ''")
+            return newtext, ''
+    return wikitext, ''
+
 def fixthis2(sections, languages, parts):
     categoriesnotfound = []
+    haschanges = False
     #print('check one')
     for onesection in sections:
         #print(sections[onesection]['title'])
@@ -24,60 +58,26 @@ def fixthis2(sections, languages, parts):
             repstring += '|' + langiso
         basicstring = repstring
         repstring += '}}'
-        newcontent = re.sub(nameAre, repstring, section['content'])
-        if section['title'] == 'κύριο όνομα':
-            if basicstring not in newcontent:
-                #print('brhka ===========',  sections[0]['title'],section['langiso'] )
-                newcontent = newcontent.rstrip() + '\n# '+ repstring + '\n\n'
-                #print(section['content'])
-        if section['content'] != newcontent:
-            #category = '[[Κατηγορία:Ανδρικά ονόματα (' + section['langname'] + ')]]'
+        newcontent = section['content']
+        hasoldstyle = re.search(nameAre,section['content'])
+        if hasoldstyle:
+            #replace it, check category
+            newcontent = re.sub(nameAre, repstring, section['content'])
+            haschanges = True
             categoryre = '\[\[Κατηγορία:Ανδρικά ονόματα \(' + section['langname'] + '\)\|*?[^\]]*]]'
-            #print(onesection, categoryre)
-            newcontent2 = re.sub(categoryre,'', newcontent)
-            if newcontent2 == newcontent:
-                categoriesnotfound.append(categoryre)
-                #print('categoriesnotfound')
+            hascategory = re.search(categoryre, newcontent)
+            if hascategory:
+                newcontent = re.sub(categoryre,'', newcontent)
             else:
-                newcontent = newcontent2
-            #print('=========== new ==========')
-            #print(newcontent)
+                categoriesnotfound.append(categoryre)
             section['content'] = newcontent
-    #print("len(sections)-1,'len(sections)-1'",len(sections)-1,'len(sections)-1')
     lastesection = sections[len(sections)-1]
     #αντικατέστησε τις κατηγορίες που δεν βρήκες μέσα στις ενότητες
+    #και ίσως έχουν μπει στο τέλος
     for categorynotfound in categoriesnotfound:
-        oldcontent = lastesection['content']
-        newcontent = re.sub(categorynotfound,'',oldcontent) 
-        lastesection['content'] = newcontent
+        newcontent = re.sub(categorynotfound,'', lastesection['content'])
+        if lastesection['content'] != newcontent:
+            haschanges = True
+            lastesection['content'] = newcontent
     #print('return sections')
-    return sections
-
-def fixthis(wikitext, languages, parts, basic):
-    #print('inside')
-    nameAAllre = nameAre + '|\[\[Κατηγορία:Ανδρικά ονόματα \('
-    b = re.search(nameAAllre, wikitext)
-    #print('inside 2')
-    if '[[Κατηγορία:Ανδρικά ονόματα (ελληνικά)]]' in wikitext and '{{ονομαΑ}}' in wikitext:
-        return wikitext.replace('[[Κατηγορία:Ανδρικά ονόματα (ελληνικά)]]', '').rstrip(), ''
-    elif b:
-        #print('found one')
-        sections = basic.getsections(wikitext, languages, parts)
-        
-        if sections[0]['garbage'] != '':
-            print('HAS GARBAGE')
-            return wikitext, sections[0]['garbage'] 
-        #if '{{τ|en|Democritus}}' in wikitext:
-            #print(sections)
-        #print(sections)
-        newsections = fixthis2(sections, languages, parts)
-        newsections = basic.fixseparators(newsections)
-        newtext = newsections[0]['content'] 
-        for xcounter in range(1,len(newsections)):
-            depth = newsections[xcounter]['depth']
-            newtext += "=" * depth + newsections[xcounter]['originaltitle'] + "=" * depth + '\n'
-            newtext += newsections[xcounter]['content']
-        newtext = newtext.rstrip()
-        #print("return newtext, ''")
-        return newtext, ''
-    return wikitext, ''
+    return sections, haschanges
