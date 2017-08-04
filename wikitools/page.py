e84c34a3d75a5337ce40f21f30ff1bcebe1a5666
Ηλιάδης Ηλίας <Xoristzatziki@users.noreply.github.com>
2017-08-04 17:23:19 +0300

    Update page.py

--- wikitools/page.py
+++ wikitools/page.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with wikitools.  If not, see <http://www.gnu.org/licenses/>.
 
+# Changes made by xoristzatziki
+
 from . import exceptions
 from . import api
 from . import internalfunctions
